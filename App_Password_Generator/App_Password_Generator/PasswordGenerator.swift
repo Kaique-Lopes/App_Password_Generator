5d427f2c3e7a3ef3d60d76c4e47b3a4abbe8bd81
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-04-25 21:46:04 -0300

    PasswordGenerator and Variables

---
+++ App_Password_Generator/App_Password_Generator/PasswordGenerator.swift
@@ -0,0 +1,21 @@
+//
+//  PasswordGenerator.swift
+//  App_Password_Generator
+//
+//  Created by Kaique Lopes on 25/04/21.
+//
+
+import Foundation
+
+class PasswordGenerator {
+    var numberOfCharacters: Int
+    var useLetters: Bool
+    var useNumbers: Bool
+    var useCapitalLetters: Bool
+    var useSpecialCharacters: Bool
+    
+    var passwords: [String] = []
+    
+    let
+    
+}
