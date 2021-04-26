3057a0500abcc7dc081bd7383629d75053055bf7
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-04-25 21:32:30 -0300

    Outlets TextFields

--- App_Password_Generator/App_Password_Generator/ViewController.swift
+++ App_Password_Generator/App_Password_Generator/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tfTotalPasswords: UITextField!
+    @IBOutlet weak var tfTotalCharacters: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
