f408d81c45882c541ab1656a9a51c9b0c9f87dcd
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-04-25 21:39:02 -0300

    function generate

--- App_Password_Generator/App_Password_Generator/PasswordsViewController.swift
+++ App_Password_Generator/App_Password_Generator/PasswordsViewController.swift
@@ -19,9 +19,10 @@ class PasswordsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    @IBAction func generate(_ sender: Any) {
+        
+    }
+    
 }
