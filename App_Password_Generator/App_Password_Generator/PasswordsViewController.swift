01ef308cce417265537a9656ffe4851ca1d0b0b7
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-04-25 21:38:05 -0300

    Variables

--- App_Password_Generator/App_Password_Generator/PasswordsViewController.swift
+++ App_Password_Generator/App_Password_Generator/PasswordsViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 class PasswordsViewController: UIViewController {
     @IBOutlet weak var tvPasswords: UITextView!
     
+    var numberOfCharacters: Int = 10
+    var numberOfPasswords: Int = 1
+    var useLetters: Bool!
+    var useNumbers: Bool!
+    var useCapitalLetters: Bool!
+    var useSpecialCharacters: Bool!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
