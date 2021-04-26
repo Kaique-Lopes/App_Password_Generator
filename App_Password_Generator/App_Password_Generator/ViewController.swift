5c82b08d55c9d164ab09f311950365398e2ec982
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-04-25 21:33:54 -0300

    Outlet UISwitch

--- App_Password_Generator/App_Password_Generator/ViewController.swift
+++ App_Password_Generator/App_Password_Generator/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var tfTotalPasswords: UITextField!
     @IBOutlet weak var tfTotalCharacters: UITextField!
+    @IBOutlet weak var swLetters: UISwitch!
+    @IBOutlet weak var swNumbers: UISwitch!
+    @IBOutlet weak var swCapitalLetters: UISwitch!
+    @IBOutlet weak var swSpecialCharacters: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
