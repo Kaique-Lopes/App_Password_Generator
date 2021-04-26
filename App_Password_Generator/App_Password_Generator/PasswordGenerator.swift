848a74952f381fbb45e5988526d0f90b123d2a18
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-04-26 20:16:50 -0300

    init

--- App_Password_Generator/App_Password_Generator/PasswordGenerator.swift
+++ App_Password_Generator/App_Password_Generator/PasswordGenerator.swift
@@ -20,4 +20,15 @@ class PasswordGenerator {
     private let numbers = "0123456789"
     private let specialCharacters = "!@#.,;/?-~´{}|\<>:>^[]*-/+$%&()"
     
+    init(numberOfCharacters: Int, useLetters: Bool, useNumbers: Bool, useCapitalLetters: Bool, useSpecialCharacters: Bool) {
+        var numchars = min(numberOfCharacters, 16)
+        numchars = max(numchars, 1)
+        
+        self.useLetters = useLetters
+        self.useNumbers = useNumbers
+        self.useCapitalLetters = useCapitalLetters
+        self.numberOfCharacters = numchars
+        self.useSpecialCharacters = useSpecialCharacters
+    }
+    
 }
