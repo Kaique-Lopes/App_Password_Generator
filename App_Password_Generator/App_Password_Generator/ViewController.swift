609ca0d24fe055800671622587c975509ff2275c
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-04-29 21:36:22 -0300

    UIStoryboardSegue

--- App_Password_Generator/App_Password_Generator/ViewController.swift
+++ App_Password_Generator/App_Password_Generator/ViewController.swift
@@ -19,7 +19,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let passwordsViewController = segue.destination
+    }
 
 }
 
